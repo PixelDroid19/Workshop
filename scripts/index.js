05c22709b96ceb377f4f3408508d846fa493b052
Damien Monasterios <monasterio13septiembre@gmail.com>
2021-08-31 21:17:28 -0500

    card coding new

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,31 @@
-products
+const items = document.getElementById("items"),
+  templateCard = document.getElementById("template-card").content,
+  fragment = document.createDocumentFragment();
+
+  document.addEventListener("DOMContentLoaded", () => {
+    GetData();
+  });
+  
+  const GetData = async () => {
+    try {
+      const res = await fetch("./Data/items.json");
+      const data = await res.json();
+  
+      //Recorre el json con map
+      data.map((Datos) => {
+        const { id, name, image, costo} = Datos;
+        templateCard.querySelector("img").setAttribute("src", image);
+        templateCard.querySelector("h5").textContent = name;
+        templateCard.querySelector("span").textContent = costo;
+        templateCard.querySelector(".card").dataset.id = id;
+        const clone = templateCard.cloneNode(true);
+        fragment.appendChild(clone);
+      });
+      // appendChild agrega un nodo al final de la lista
+      items.appendChild(fragment);
+  
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
